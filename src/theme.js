ff2791112118e26a67686eda6c3fd3a9f8fa5b67
unknown <ndson1703@gmail.com>
2023-11-19 21:16:26 +0700

    add prettier config

---
+++ src/theme.js
@@ -0,0 +1,20 @@
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+import { teal, deepOrange, cyan, orange } from '@mui/material/colors'
+
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange
+      }
+    },
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: orange
+      }
+    }
+  }
+})
+export default theme
