44ec7016966cc8a4e5ccdc41941a3bab3f3f4b9d
sonng17 <ndson1703@gmail.com>
2024-02-21 02:54:58 +0700

    fix build mode dev and production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,8 @@
-export const API_ROOT = 'https://trello-api1-64m4.onrender.com'
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api1-64m4.onrender.com/'
+}
+export const API_ROOT = apiRoot
