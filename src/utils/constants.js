371c393a277b389d6163b218d930babd014c1f6e
sonng17 <ndson1703@gmail.com>
2024-02-21 03:15:36 +0700

    fix apiRoot

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,6 @@ if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8017'
 }
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'https://trello-api1-64m4.onrender.com/'
+  apiRoot = 'https://trello-api1-64m4.onrender.com'
 }
 export const API_ROOT = apiRoot
